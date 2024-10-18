77c413773604aacd86b53ffbb2c6f7c0d04dea92
rajeshRoul <roul.rajesh28@gmail.com>
2024-10-19 05:03:22 +0530

    Updates

--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const getTasksFromCache = () => {
-    const tasks = localStorage.getItem('tasks');
-    return tasks ? JSON.parse(tasks) : null;
-  };
-  
-  export const setTasksToCache = (tasks) => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  };
-  
